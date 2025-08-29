7de0ac8a8fb5b90176c4b5cb76173c760726d907
jasirkonnola <jasirhassan.894315@gmail.com>
2025-08-29 16:21:27 +0530

    Update question model, view logic, and homepage layout; add migrations

--- edumentor/edubase/urls.py
+++ edumentor/edubase/urls.py
@@ -6,4 +6,8 @@ app_name = 'edubase'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+
+    #CRUD Function
+    path('questions/', views.QuestionListView.as_view(), name='question-lists'),
+
 ]
--- edumentor/edubase/views.py
+++ edumentor/edubase/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Question
+
 
 def home(request):
     return render(request, 'home.html')
@@ -6,3 +9,9 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+#CRUD Function
+class QuestionListView(ListView):
+    model = Question
+    context_object_name = 'questions'
+    template_name = 'questions.html'  # Make sure this matches your actual template filename
+    ordering = ['-created_at']  # Orders questions by creation date, newest first+
